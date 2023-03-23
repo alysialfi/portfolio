c9c4bd052cdc680c5bef447a27d60359beb03185
alfi <alfi@domainesia.com>
2023-03-23 23:28:10 +0700

    feat: (components/Haeder) create header page & import supporting images

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="relative w-full h-screen">
+        <img class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-0" src="../assets/images/header/cat-small.svg" alt="a lined big cat body">
+        <div class="absolute top-96 left-1/4">
+            <img src="../assets/images/header/mustache-left.svg" alt="" srcset="">
+        </div>
+        <div>
+            <div class="absolute top-64 left-1/3 translate-x-3/4">
+                <img src="../assets/images/header/eye-blue-outer.svg" alt="" srcset="">
+            </div>
+            <div class="absolute top-72 left-1/3 translate-x-[175%]">
+                <img src="../assets/images/header/eye-blue-inner.svg" alt="" srcset="">
+            </div>
+        </div>
+        <div class="absolute top-96 left-1/2">
+            <img src="../assets/images/header/nose.svg" alt="" srcset="">
+        </div>
+        <div>
+            <div class="absolute top-64 left-2/3 -translate-x-[125%]">
+                <img src="../assets/images/header/eye-red-outer.svg" alt="" srcset="">
+            </div>
+            <div class="absolute top-72 left-2/3 -translate-x-[175%]">
+                <img src="../assets/images/header/eye-red-inner.svg" alt="" srcset="">
+            </div>
+        </div>
+        <div class="absolute top-[350px] right-1/4 translate-x-1/3">
+            <img src="../assets/images/header/mustache-right.svg" alt="" srcset="">
+        </div>
+        <!-- <div class="flex w-full h-screen justify-center items-center">
+            <span>
+                she/her <br>
+                Indonesian and <br>
+                a wanderlust
+            </span>
+            <h1>Hi! It's <span class="text-2xl">Alfi</span></h1>
+            <span>
+                so called front end developer <br>
+                sometimes do back end-ish <br>
+                things
+            </span>
+        </div> -->
+        <div class="absolute bottom-16 left-16">
+            <h3 class="text-base font-extralight">
+                she/her <br>
+                Indonesian and <br>
+                a wanderlust
+            </h3>
+        </div>
+        <div class="absolute bottom-8 left-1/2 -translate-x-1/2 translate-y-0">
+            <h1 class="text-[142px] font-extralight">Hi! It's <span class="text-[228px] font-semibold">Alfi</span></h1>
+        </div>
+        <div class="absolute top-1/4 right-16">
+            <h3 class="text-base font-extralight">
+                so called front end developer <br>
+                sometimes do back end-ish <br>
+                things
+            </h3>
+        </div>
+    </div>
+</template>
