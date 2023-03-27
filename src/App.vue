93a66cdcc37057e8a748408b6e46681bf39f7864
alfi <alfi@domainesia.com>
2023-03-27 22:21:45 +0700

    feat: (components/Searching, src/App): create & import searching component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Header from './components/Header.vue'
+    import Searching from './components/Searching.vue'
 </script>
 
 <template>
     <Header/>
+    <Searching/>
 </template>
 
 <style scoped>
---
+++ src/components/Searching.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div id="Searching" class="w-full h-screen flex justify-center items-center">
+        <div id="typed-text" ref="typedText" class="border border-b-0 border-l-0 border-t-0 border-r-white whitespace-nowrap overflow-hidden">
+            <p id="text" class="text-lg text-center">these are pretty much what i do for living...</p>
+        </div>
+    </div>
+</template>
