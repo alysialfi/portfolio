104413d522d0e9e71fb3bcd16d6ad7a4035e6be5
alfi <alfi@domainesia.com>
2023-04-04 22:29:29 +0700

    feat: (components/Component, src/App) create & import component page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Header from './components/Header.vue'
     import Searching from './components/Searching.vue'
+    import Component from './components/Component.vue'
 </script>
 
 <template>
     <Header/>
     <Searching/>
+    <Component/>
 </template>
 
 <style scoped>
---
+++ src/components/Component.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="w-full h-screen">
+        <h3>Connecting components in a page? Why not</h3>
+    </div>
+</template>
