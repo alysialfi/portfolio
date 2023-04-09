ac2c11ae071956b8a61ae98a820e4af10c6578cf
alfi <alfi@domainesia.com>
2023-04-09 11:16:26 +0700

    feat: (components/Component) provide dummy product data

--- src/components/Component.vue
+++ src/components/Component.vue
@@ -1,4 +1,36 @@
 <script setup>
+    import { onMounted, ref, reactive } from 'vue';
+
+    const isModalOpen = ref(false)
+    const states = reactive({
+        products: [
+            {
+                thumbnail: '../../assets/images/products/red-backpack.png',
+                title: 'Red Backpack',
+                category: 'Woman bag',
+                material: '80% leather 20% cotton',
+                selectedColor: 'red',
+                price: 30
+            },
+            {
+                thumbnail: '../../assets/images/products/anyaman-handbag.png',
+                title: 'Anyaman Hand Bag',
+                category: 'Woman bag',
+                material: '80% leather 20% cotton',
+                selectedColor: 'brown',
+                price: 50
+            },
+            {
+                thumbnail: '../../assets/images/products/navy-backpack.png',
+                title: 'Navy Backpack',
+                category: 'Man bag',
+                material: '80% cotton 20% polyester',
+                selectedColor: 'navy',
+                price: 30
+            }
+        ],
+        productColors: [ 'blue', 'red', 'brown', 'yellow' ]
+    })
 </script>
 
 <template>
